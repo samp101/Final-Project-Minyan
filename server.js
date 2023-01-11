3d1a8aabb8cf0c9555d0c4b6e6a036ca29cafa23
samp101 <shmpos@gmail.com>
2023-01-12 00:01:50 +0200

    changes to GI

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(router)
 // app.use(express.static(path.join(__dirname, "./client/build")));
 app.use("/", express.static(path.join(__dirname, "./client/build")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
